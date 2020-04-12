d755ca72129fe0ace8cdce1e72bbc68f1d305c67
javier-camilo <camiloviloriatovar@gmail.com>
2020-04-12 18:33:51 -0500

    nueva adicion

--- ExpoSoftware/ClientApp/src/app/app.module.ts
+++ ExpoSoftware/ClientApp/src/app/app.module.ts
@@ -16,8 +16,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 
 
@@ -40,6 +41,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
